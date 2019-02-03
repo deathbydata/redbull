9e4143d441b57a7539fe9a092d730ba604bfd6b0
Josh Muncke <joshxthomas@gmail.com>
2019-02-03 12:35:33 -0800

    Updated palette options

--- R/palettes.R
+++ R/palettes.R
@@ -1,9 +1,9 @@
 #' Palette definitions for redbull package
 redbull_palettes <- list(
-  `redbull` = redbull_cols(),
-  `rbprimary` = redbull_cols("red", "blue", "yellow", "grey"),
-  `rbcool` = redbull_cols("blue", "light blue"),
-  `rbwarm` = redbull_cols("red", "pink"),
+  `redbull` = redbull_cols("red", "blue", "yellow", "light grey"),
+  `rbfull` = redbull_cols(),
+  `rbcool` = redbull_cols("blue", "lightest blue"),
+  `rbwarm` = redbull_cols("red", "lightest red"),
   `rbyellowgrey` = redbull_cols("yellow", "grey"),
   `bloomberg` = bloomberg_cols("blue", "green", "pink", "yellow", "hot pink", "dark blue", "orange", "dark green"),
   `bloombergfull` = bloomberg_cols()
